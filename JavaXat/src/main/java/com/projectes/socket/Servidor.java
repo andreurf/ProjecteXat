bc59e2366040a12848862efe93544e42b7e7078d
203-38\andreu.reyfor.dam_in <andreu.reyfor.dam@insalfonscostafreda.cat>
2024-04-08 15:36:10 +0200

    Initial Commit

---
+++ JavaXat/src/main/java/com/projectes/socket/Servidor.java
@@ -0,0 +1,14 @@
+package com.projectes.socket;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Andreu i Quim
+ */
+public class Servidor {
+    
+}
