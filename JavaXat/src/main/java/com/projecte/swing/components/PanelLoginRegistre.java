88c31f4b6a993444b09a4b9ee59f516f938c555a
203-38\andreu.reyfor.dam_in <andreu.reyfor.dam@insalfonscostafreda.cat>
2024-04-10 17:13:18 +0200

    Registre funcional(falta validacions) + canvis de text a inici sessio i registre

--- JavaXat/src/main/java/com/projecte/swing/components/PanelLoginRegistre.java
+++ JavaXat/src/main/java/com/projecte/swing/components/PanelLoginRegistre.java
@@ -1,16 +1,17 @@
 package com.projecte.swing.components;
 
+import com.projecte.models.Usuari;
+import com.projecte.service.MongoDBConexio;
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Font;
-import java.awt.GradientPaint;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import net.miginfocom.swing.MigLayout;
 
 /**
@@ -19,8 +20,11 @@ import net.miginfocom.swing.MigLayout;
  */
 public class PanelLoginRegistre extends javax.swing.JLayeredPane {
 
+    private MongoDBConexio dbConnection;
+
     public PanelLoginRegistre() {
         initComponents();
+        dbConnection = new MongoDBConexio();
         initRegistre();
         initLogin();
         //login.setVisible(false);
@@ -34,31 +38,47 @@ public class PanelLoginRegistre extends javax.swing.JLayeredPane {
         label.setFont(new Font("sanserif", 1, 30));
         label.setForeground(new Color(166, 35, 35));
         registre.add(label);
-        
+
         // TextField Usuari
         JTextFieldPersonalitzat txtUser = new JTextFieldPersonalitzat();
         txtUser.setPrefixIcon(new ImageIcon(getClass().getResource("/user.png")));
         txtUser.setHint("Usuari");
         registre.add(txtUser, "w 60%");
-        
+
         //TextField Email
         JTextFieldPersonalitzat txtEmail = new JTextFieldPersonalitzat();
         txtEmail.setPrefixIcon(new ImageIcon(getClass().getResource("/mail.png")));
         txtEmail.setHint("Correu Electrònic");
         registre.add(txtEmail, "w 60%");
-        
+
         //TextField Contrassenya
         JTextFieldPersonalitzat txtPass = new JTextFieldPersonalitzat();
         txtPass.setPrefixIcon(new ImageIcon(getClass().getResource("/pass.png")));
         txtPass.setHint("Contrassenya");
         registre.add(txtPass, "w 60%");
-        
+
         Boto cmd = new Boto();
         cmd.setBackground(new Color(166, 35, 35));
-        cmd.setForeground(new Color(250,250,250));
+        cmd.setForeground(new Color(250, 250, 250));
         cmd.setText("REGISTRAR-SE");
+        cmd.addActionListener((ActionEvent e) -> {
+            String usuari = txtUser.getText();
+            String email = txtEmail.getText();
+            String contrassenya = txtPass.getText();
+            Usuari newUser = new Usuari(usuari, contrassenya, email);
+
+            try {
+                dbConnection.insertUser(newUser);
+                JOptionPane.showMessageDialog(null, "Usuario registrado correctamente");
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, "Error al registrar el usuario: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            }
+
+        });
+        ;
+
         registre.add(cmd, "w 40%, h 40");
-        
+
     }
 
     private void initLogin() {
@@ -67,44 +87,44 @@ public class PanelLoginRegistre extends javax.swing.JLayeredPane {
         label.setFont(new Font("sanserif", 1, 30));
         label.setForeground(new Color(166, 35, 35));
         login.add(label);
-        
+
         // TextField Usuari
         JTextFieldPersonalitzat txtUser = new JTextFieldPersonalitzat();
         txtUser.setPrefixIcon(new ImageIcon(getClass().getResource("/user.png")));
         txtUser.setHint("Usuari");
         login.add(txtUser, "w 60%");
-        
+
         //TextField Contrassenya
         JTextFieldPersonalitzat txtPass = new JTextFieldPersonalitzat();
         txtPass.setPrefixIcon(new ImageIcon(getClass().getResource("/pass.png")));
         txtPass.setHint("Contrassenya");
         login.add(txtPass, "w 60%");
         JButton cmdRenovarPass = new JButton("Has olvidat la contrassenya?");
-        cmdRenovarPass.setForeground(new Color(100,100,100));
+        cmdRenovarPass.setForeground(new Color(100, 100, 100));
         cmdRenovarPass.setBorderPainted(false);
-        cmdRenovarPass.setFont(new Font("sansserif",1,12));
+        cmdRenovarPass.setFont(new Font("sansserif", 1, 12));
         cmdRenovarPass.setContentAreaFilled(false);
         cmdRenovarPass.setCursor(new Cursor(Cursor.HAND_CURSOR));
         login.add(cmdRenovarPass);
-        
+
         Boto cmd = new Boto();
         cmd.setBackground(new Color(166, 35, 35));
-        cmd.setForeground(new Color(250,250,250));
+        cmd.setForeground(new Color(250, 250, 250));
         cmd.setText("Inciar Sessió");
         login.add(cmd, "w 40%, h 40");
-        
+
     }
-    
-    public void mostrarRegistre(boolean mostrar){
-        if(mostrar){
+
+    public void mostrarRegistre(boolean mostrar) {
+        if (mostrar) {
             registre.setVisible(true);
             login.setVisible(false);
-        }else{
+        } else {
             registre.setVisible(false);
             login.setVisible(true);
         }
     }
-    
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
