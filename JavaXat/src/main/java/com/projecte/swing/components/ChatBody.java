62b2e542f659b8562d0262abd23489307647ae2a
203-38\andreu.reyfor.dam_in <andreu.reyfor.dam@insalfonscostafreda.cat>
2024-04-23 19:04:01 +0200

    Inici part 3 chatText

---
+++ JavaXat/src/main/java/com/projecte/swing/components/ChatBody.java
@@ -0,0 +1,63 @@
+package com.projecte.swing.components;
+
+import java.awt.Color;
+import javax.swing.JScrollBar;
+import net.miginfocom.swing.MigLayout;
+
+/**
+ *
+ * @author andreu i quim
+ */
+public class ChatBody extends javax.swing.JPanel {
+
+    public ChatBody() {
+        initComponents();
+        init();
+    }
+
+    private void init() {
+        body.setLayout(new MigLayout("fillx"));
+        sp.setVerticalScrollBar(new JScrollBar());
+        sp.getVerticalScrollBar().setBackground(Color.WHITE);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        sp = new javax.swing.JScrollPane();
+        body = new javax.swing.JPanel();
+
+        body.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout bodyLayout = new javax.swing.GroupLayout(body);
+        body.setLayout(bodyLayout);
+        bodyLayout.setHorizontalGroup(
+            bodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 824, Short.MAX_VALUE)
+        );
+        bodyLayout.setVerticalGroup(
+            bodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 553, Short.MAX_VALUE)
+        );
+
+        sp.setViewportView(body);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(sp)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(sp)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel body;
+    private javax.swing.JScrollPane sp;
+    // End of variables declaration//GEN-END:variables
+}
